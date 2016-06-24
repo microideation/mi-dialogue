763ee27f7319fb45fde4aa973a34ee19fba75199
Sandheep Menon <gr.sandeep1@gmail.com>
2016-06-24 10:06:53 +0530

    removed the reference to the SampleService

--- mi-dialogue-core/src/test/java/com/microideation/app/dialogue/MIDialogueApplicationTests.java
+++ mi-dialogue-core/src/test/java/com/microideation/app/dialogue/MIDialogueApplicationTests.java
@@ -1,9 +1,7 @@
 package com.microideation.app.dialogue;
 
-import com.microideation.app.dialogue.sample.service.SampleService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -13,25 +11,9 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @WebAppConfiguration
 public class MIDialogueApplicationTests {
 
-    /*@Autowired
-    private SampleService sampleService;
-*/
+
     @Test
     public void contextLoad() {}
 
-    /*@Test
-    public void testRedis() {
-
-        sampleService.publishToRedis();
-
-    }
-
-
-    @Test
-    public void testRabbit() {
-
-        sampleService.publishToRabbit();
-
-    }*/
 
 }
